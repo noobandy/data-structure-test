6648e7ece8a0c239daba095854b74fa1ac3fb114
noobandy <noobandy1364@gmail.com>
2015-09-22 15:43:10 +0530

    Arithmetic Expressions

--- src/org/app/ds/applications/ArithmeticExpression.java
+++ src/org/app/ds/applications/ArithmeticExpression.java
@@ -4,7 +4,11 @@
 package org.app.ds.applications;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author anandm
@@ -12,172 +16,305 @@ import java.util.List;
  */
 public class ArithmeticExpression {
 
-	private String expression;
-	private List<String> tokens;
+    static Map<String, Integer> precedenceMap = new HashMap<String, Integer>();
 
-	/**
-	 * @param expression
-	 */
-	public ArithmeticExpression(String expression) {
-		super();
-		validate(expression);
-		this.expression = expression;
-		this.tokens = tokens(expression);
-	}
+    static {
+        precedenceMap.put("(", 8);
+        precedenceMap.put(")", 8);
+        precedenceMap.put("/", 7);
+        precedenceMap.put("*", 7);
+        precedenceMap.put("+", 6);
+        precedenceMap.put("-", 6);
+    }
 
-	private interface Component {
-		enum Type {
-			OPERATOR, LITERAL;
-		}
+    private enum Type {
+        OPERATOR, LITERAL;
+    }
 
-		Type getType();
+    private static boolean isOperator(char token) {
+        return (token == '*' || token == '/' || token == '(' || token == ')'
+                || token == '+' || token == '-');
+    }
 
-		String getValue();
+    private class Component {
 
-		int getPrecedence();
-	}
+        private Type type;
 
-	private class Literal implements Component {
+        private String value;
 
-		private String value;
+        private int precedence;
 
-		/**
-		 * @param value
-		 */
-		public Literal(String value) {
-			super();
-			this.value = value;
-		}
+        private Component(String token) {
+            this.value = token;
+            if (ArithmeticExpression.isOperator(token.charAt(0))) {
+                type = Type.OPERATOR;
+                precedence = precedenceMap.get(token);
+            }
+            else {
+                type = Type.LITERAL;
+                precedence = -1;
+            }
 
-		@Override
-		public Type getType() {
+        }
 
-			return Type.LITERAL;
-		}
+        public boolean isOpeningParenthesis() {
+            return isOperator() && value.equals("(");
+        }
 
-		@Override
-		public String getValue() {
+        public boolean isClosingParenthesis() {
+            return isOperator() && value.equals(")");
+        }
 
-			return value;
-		}
+        public boolean isOperator() {
+            return type == Type.OPERATOR;
+        }
 
-		@Override
-		public int getPrecedence() {
-			throw new UnsupportedOperationException();
-		}
+        public Component operation(Component a, Component b) {
+            Double aValue = Double.parseDouble(a.value);
+            Double bValue = Double.parseDouble(b.value);
+            if (value.equals("*")) {
+                return new Component(String.valueOf(aValue * bValue));
+            }
 
-	}
-	
-	private class Operator implements Component {
+            if (value.equals("*")) {
+                return new Component(String.valueOf(aValue * bValue));
+            }
 
-		private String value;
-		private int precedence;
+            if (value.equals("/")) {
+                return new Component(String.valueOf(aValue / bValue));
+            }
 
-		/**
-		 * @param value
-		 */
-		public Literal(String value) {
-			super();
-			this.value = value;
-			if(value == "(")
-		}
+            if (value.equals("+")) {
+                return new Component(String.valueOf(aValue + bValue));
+            }
 
-		@Override
-		public Type getType() {
+            if (value.equals("-")) {
+                return new Component(String.valueOf(aValue - bValue));
+            }
 
-			return Type.LITERAL;
-		}
+            return null;
+        }
 
-		@Override
-		public String getValue() {
+    }
 
-			return value;
-		}
+    private class ComponentScanner {
 
-		@Override
-		public int getPrecedence() {
-			throw new UnsupportedOperationException();
-		}
-		
-	}
+        private String expression;
 
-	private private static boolean validate(String expression) {
-		return true;
-	}
+        private int position;
 
-	private static List<String> tokens(String expression) {
+        /**
+         * @param expression
+         */
+        public ComponentScanner(String expression) {
+            this.expression = expression;
+            position = 0;
+        }
 
-		List<String> tokens = new ArrayList<String>();
+        public Component nextComponent() {
 
-		int i = 0;
-		int start = 0;
+            StringBuilder tokenBuilder = new StringBuilder();
 
-		while (i < expression.length()) {
+            while (true) {
 
-			if (isOperator(expression.charAt(i))) {
+                if (position < expression.length()) {
 
-				if (start == i) {
-					tokens.add(expression.substring(i, i + 1));
+                    char token = expression.charAt(position);
 
-				} else {
-					tokens.add(expression.substring(start, i));
-					tokens.add(expression.substring(i, i + 1));
-				}
+                    if (Character.isWhitespace(token)) {
+                        if (tokenBuilder.length() > 0) {
+                            return new Component(tokenBuilder.toString());
+                        }
 
-				i++;
+                        position++;
+                    }
+                    else {
+                        if (isOperator(token)) {
+                            if (tokenBuilder.length() > 0) {
+                                return new Component(tokenBuilder.toString());
+                            }
+                            else {
+                                position++;
+                                return new Component(Character.toString(token));
+                            }
 
-				start = i;
+                        }
+                        else {
+                            tokenBuilder.append(token);
+                            position++;
+                        }
+                    }
+                }
+                else {
+                    break;
+                }
 
-			} else {
-				i++;
-			}
+            }
 
-		}
+            return null;
 
-		if (start != i) {
-			tokens.add(expression.substring(start, expression.length()));
-		}
+        }
+    }
 
-		return tokens;
-	}
+    private String expression;
 
-	private static boolean isOperator(char token) {
-		return (token == '*' || token == '/' || token == '(' || token == ')'
-				|| token == '+' || token == '-');
-	}
+    private List<Component> components;
 
-	public String postFixExpression() {
+    /**
+     * @param expression
+     */
+    public ArithmeticExpression(String expression) {
+        super();
+        this.expression = expression;
+        this.components = components(this.expression);
+    }
 
-		for (String iterable_element : iterable) {
+    private List<Component> components(String expression) {
 
-		}
+        List<Component> components = new ArrayList<Component>();
 
-		return null;
-	}
+        ComponentScanner componentScanner = new ComponentScanner(expression);
 
-	public String preFixExpression() {
-		return null;
-	}
+        Component component = null;
+        while ((component = componentScanner.nextComponent()) != null) {
+            // System.out.println(component.value);
+            components.add(component);
+        }
 
-	public String getExpression() {
-		return expression;
-	}
+        return components;
+    }
 
-	public double evaluate() {
-		return 0.0;
-	}
+    private List<Component> postFix(List<Component> components) {
+        Stack<Component> operatorStack = new Stack<Component>();
 
-	@Override
-	public String toString() {
-		return "ArithmeticExpression [expression=" + expression + ", tokens="
-				+ tokens + "]";
-	}
+        operatorStack.push(new Component("("));
 
-	public static void main(String[] args) {
-		ArithmeticExpression arithmeticExpression = new ArithmeticExpression(
-				"((3+5.43)*(4-2))/5");
+        List<Component> postFixComponents = new ArrayList<Component>();
 
-		System.out.println(arithmeticExpression);
-	}
+        List<Component> tempComponents = new ArrayList<Component>(components);
+        tempComponents.add(new Component(")"));
 
+        for (Component component : tempComponents) {
+            if (component.isOperator()) {
+                if (component.isOpeningParenthesis()) {
+                    // opening parenthesis push on operator stack
+                    operatorStack.push(component);
+                }
+                else {
+                    if (component.isClosingParenthesis()) {
+
+                        // closing parenthesis
+                        // pop and add to postfix untill we get a closing
+                        // parenthesis
+
+                        while (!operatorStack.isEmpty()
+                                && !operatorStack.peek().isOpeningParenthesis()) {
+                            postFixComponents.add(operatorStack.pop());
+                        }
+                        // pop closing parenthesis
+                        operatorStack.pop();
+                    }
+                    else {
+                        // while there are low precedence operators on top of
+                        // stack
+                        // pop and add to post fix
+                        while (!operatorStack.isEmpty()
+                                && operatorStack.peek().precedence < component.precedence) {
+                            postFixComponents.add(operatorStack.pop());
+                        }
+                        // push component on stack
+                        operatorStack.push(component);
+                    }
+                }
+            }
+            else {
+                postFixComponents.add(component);
+            }
+
+        }
+
+        return postFixComponents;
+
+    }
+
+    private List<Component> preFix(List<Component> components) {
+
+        List<Component> tempComponents = new ArrayList<Component>(components);
+
+        Collections.reverse(tempComponents);
+
+        for (Component component : tempComponents) {
+
+            if (component.isOpeningParenthesis()) {
+                component.value = ")";
+            }
+            else {
+                if (component.isClosingParenthesis()) {
+                    component.value = "(";
+                }
+            }
+        }
+
+        return postFix(tempComponents);
+
+    }
+
+    public String postFixExpression() {
+
+        StringBuilder postFixExpressionBuilder = new StringBuilder();
+        for (Component component : postFix(components)) {
+            postFixExpressionBuilder.append(component.value).append(" ");
+        }
+        return postFixExpressionBuilder.toString().trim();
+    }
+
+    public String preFixExpression() {
+        StringBuilder preFixExpressionBuilder = new StringBuilder();
+        for (Component component : preFix(components)) {
+            preFixExpressionBuilder.append(component.value).append(" ");
+        }
+        return preFixExpressionBuilder.toString().trim();
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public double evaluate() {
+        Stack<Component> stack = new Stack<Component>();
+
+        List<Component> postFix = postFix(components);
+
+        for (Component component : postFix) {
+            if (component.isOperator()) {
+                Component b = stack.pop();
+
+                Component a = stack.pop();
+
+                stack.push(component.operation(a, b));
+            }
+            else {
+                stack.push(component);
+            }
+        }
+
+        return Double.parseDouble(stack.pop().value);
+    }
+
+    @Override
+    public String toString() {
+        return "ArithmeticExpression [expression=" + expression + "]";
+    }
+
+    public static void main(String[] args) {
+        ArithmeticExpression arithmeticExpression = new ArithmeticExpression(
+                " ( (3+ 5.43) * (4-2))/5 ");
+
+        System.out.println(arithmeticExpression.postFixExpression());
+
+        System.out.println(arithmeticExpression.preFixExpression());
+
+        System.out
+                .println(arithmeticExpression.evaluate() == ((3 + 5.43) * (4 - 2)) / 5);
+    }
 }
